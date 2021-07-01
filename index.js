13faa8c888c8bce2e22eca0e5ed92157c6a01da1
Foxunderground <umerirfan1205@gmail.com>
2021-07-02 01:03:06 +0500

    implemented sending data back to node with loop

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.post("/submit", (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+	console.log(username, password);
+
+	res.sendStatus(200);
 });
 
 let port = process.env.PORT || 8081;
